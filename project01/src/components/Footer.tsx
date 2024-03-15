c69165eddd7cbb3962ec27cd23d250bd90226cc5
kaisiq <dahudohu@gmail.com>
2024-03-15 18:14:17 +0200

    Add sample aboutus, contact, faq pages and fix footer

--- project01/src/components/Footer.tsx
+++ project01/src/components/Footer.tsx
@@ -4,21 +4,30 @@ import Link from "next/link";
 export function Footer() {
 	return (
 		<footer className="flex w-full shrink-0 flex-col items-center gap-2 border-t px-4 py-4 sm:flex-row sm:py-6 md:px-6">
-			<div className="flex items-center gap-2">
+			<Link href="/" className="flex items-center gap-2">
 				<BookMarkedIcon className="h-6 w-6" />
 				<span className="text-sm font-semibold tracking-wide">Две/Трети</span>
-			</div>
+			</Link>
 			<p className="text-xs text-gray-500 dark:text-gray-400">
 				Още нямаме адрес, хаха
 			</p>
 			<nav className="flex gap-4 sm:ml-auto sm:gap-6">
-				<Link className="text-xs underline-offset-4 hover:underline" href="#">
+				<Link
+					className="text-xs underline-offset-4 hover:underline"
+					href="/aboutus"
+				>
 					About Us
 				</Link>
-				<Link className="text-xs underline-offset-4 hover:underline" href="#">
+				<Link
+					className="text-xs underline-offset-4 hover:underline"
+					href="/contact"
+				>
 					Contact
 				</Link>
-				<Link className="text-xs underline-offset-4 hover:underline" href="#">
+				<Link
+					className="text-xs underline-offset-4 hover:underline"
+					href="/faq"
+				>
 					FAQs
 				</Link>
 			</nav>
